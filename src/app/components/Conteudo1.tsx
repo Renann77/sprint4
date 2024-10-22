0ac2abc44d648b81ce13b0c225064daa6ea6eceb
Renann77 <renan.dorneles.potencial@gmail.com>
2024-10-22 15:20:13 -0300

    Criando projeto sprint4

---
+++ src/app/components/Conteudo1.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Conteudo1() {
+  return (
+    <div></div>
+  )
+}
---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <footer>
+            <p>Lri.company Todos Direitos Reservados 2024</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
