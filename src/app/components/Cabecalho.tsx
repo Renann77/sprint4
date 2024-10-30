69e1fc76bc698494f4c6d40cc2b9428d38d2c173
Renann77 <renan.dorneles.potencial@gmail.com>
2024-10-30 09:37:55 -0300

    colocando icones

--- src/app/components/Cabecalho.tsx
+++ src/app/components/Cabecalho.tsx
@@ -7,7 +7,6 @@ export default function Cabecalho() {
         <header className="flex flex-col items-center bg-gradient-to-r from-[#061f2c] to-[#0c3a4a] p-5">
              
             <Image 
-
                 src={logo}  
                 alt="Logo da Lri Companny" 
                  className="w-[300px] h-[300px] rounded-full shadow-lg transition-transform transition-shadow duration-400 ease-in-out transform hover:scale-110 hover:rotate-[5deg] hover:shadow-[0_16px_32px_rgba(243,213,132,0.5)]"
--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
+import { FaHome } from "react-icons/fa";
+import { GrProductHunt } from "react-icons/gr";
+import { FaCartShopping } from "react-icons/fa6";
+import { FaCirclePlus } from "react-icons/fa6";
 
 export default function Menu(){
 
     return(
         <nav className="flex">
             <ul className="flex gap-7 links">
-                <li><Link href={'/'}>Home</Link></li>
-                <li><Link href={'/produtos'}>Produtos</Link></li>
-                <li><Link href={'/comprra'}>Compra De Peças </Link></li>
-                <li><Link href={'/produtos/cad-produtos'}>Cadastro de Produtos</Link></li>
+                <li><FaHome className="gap-2 w-9"/><Link href={'/'}>Home</Link></li>
+                <li><GrProductHunt className="gap-2 w-9" /><Link href={'/produtos'}>Produtos</Link></li>
+                <li><FaCartShopping className="gap-2 w-9" /><Link href={'/comprra'}>Compra De Peças </Link></li>
+                <li><FaCirclePlus className="gap-2 w-9"/><Link href={'/produtos/cad-produtos'}>Cadastro de Produtos</Link></li>
             </ul>
         </nav>
     )
